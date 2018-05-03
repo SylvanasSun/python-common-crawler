f375da75098a50a1feaa2ee936a2755179249e97
SylvanasSun <sylvanassun_xtz@163.com>
2018-05-03 23:21:22 +0800

    Add unit test of common_crawler.http.aiohttp.AioHttpClient

---
+++ tests/test_http.py
@@ -0,0 +1,89 @@
+import asyncio
+import unittest
+
+from common_crawler.http.aiohttp import AioHttpClient
+
+_TARGET_URL = 'https://www.python.org'
+
+
+def _run(coro):
+    return asyncio.get_event_loop().run_until_complete(coro)
+
+
+class TestAioHttpClient(unittest.TestCase):
+    """Test for common_crawler.http.aiohttp.AioHttpClient"""
+
+    def setUp(self):
+        self.client = AioHttpClient()
+
+    def test_request(self):
+        async def work():
+            resp = await self.client.request(method='GET', url=_TARGET_URL)
+            self.assertEqual(resp.status, 200)
+
+        _run(work())
+
+    def test_get(self):
+        async def work():
+            resp = await self.client.get(url=_TARGET_URL)
+            self.assertEqual(resp.status, 200)
+
+        _run(work())
+
+    def test_post(self):
+        async def work():
+            resp = await self.client.post(url=_TARGET_URL)
+            self.assertEqual(resp.status, 403)
+
+        _run(work())
+
+    def test_put(self):
+        async def work():
+            resp = await self.client.put(url=_TARGET_URL)
+            self.assertEqual(resp.status, 403)
+
+        _run(work())
+
+    def test_delete(self):
+        async def work():
+            resp = await self.client.delete(url=_TARGET_URL)
+            self.assertEqual(resp.status, 403)
+
+        _run(work())
+
+    def test_options(self):
+        async def work():
+            resp = await self.client.options(url=_TARGET_URL)
+            self.assertEqual(resp.status, 200)
+
+        _run(work())
+
+    def test_head(self):
+        async def work():
+            resp = await self.client.head(url=_TARGET_URL)
+            self.assertEqual(resp.status, 200)
+
+        _run(work())
+
+    def test_patch(self):
+        async def work():
+            resp = await self.client.patch(url=_TARGET_URL)
+            self.assertEqual(resp.status, 403)
+
+        _run(work())
+
+    def test_get_from_kwargs(self):
+        kw = {'a': 0, 'b': 1}
+        r = self.client._get_from_kwargs(kw, 'a', None)
+        self.assertEqual(r, kw['a'])
+        r = self.client._get_from_kwargs(kw, 'b', None)
+        self.assertEqual(r, kw['b'])
+        r = self.client._get_from_kwargs(kw, 'c', None)
+        self.assertEqual(r, None)
+
+    def tearDown(self):
+        self.client.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
